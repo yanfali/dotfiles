f1831d4b257b7fccfcd70d03e083a2f0ecca4d8a
Yan-Fa Li <yanfali@gmail.com>
2014-11-07 16:18:34 -0800

    Add scripts
    - bootstrap npm packages globally
    - add gvimrc
    - add httpie

---
+++ scripts/httpie.sh
@@ -0,0 +1,4 @@
+
+#!/usr/bin/env bash
+echo "Installing httpie"
+sudo pip install httpie
---
+++ scripts/npm.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# NPM Updates
+echo "Updating NPM"
+npm install -g npm@latest
+echo "Installing jsonlint, yo and grunt-cli"
+npm install -g jsonlint yo grunt-cli

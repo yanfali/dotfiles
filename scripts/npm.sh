48b1ff857fb6c2a43012348ed97f6bb21a481fd8
Yan-Fa Li <yanfali@gmail.com>
2014-11-11 07:36:16 -0800

    Add ack, nmap and bower

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -3,4 +3,4 @@
 echo "Updating NPM"
 npm install -g npm@latest
 echo "Installing jsonlint, yo and grunt-cli"
-npm install -g jsonlint yo jshint js-beautify grunt-cli traceur
+npm install -g jsonlint yo jshint js-beautify grunt-cli traceur bower
